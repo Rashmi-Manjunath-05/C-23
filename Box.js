195ad95e8ad20fcc2b094d0614f6aac781156b63
Rashmi-Manjunath-05 <76782939+Rashmi-Manjunath-05@users.noreply.github.com>
2021-03-03 11:38:26 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box{
+    constuctor(){
+        var options={
+            restitution:1.0
+        }
+        this.body=Bodies.rectangle(200,100,50,50,options);
+        this.width=50;
+        this.height=50;
+        World.add(world,this.body);
+    }
+    display(){
+         var pos = this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
